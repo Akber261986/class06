d40df75127604b1cfb396854bd46c4e1be97bf5a
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2024-10-13 23:33:17 +0500

    fixed some error

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,10 @@
+'use client';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center mt-6">
       <h1 className="text-6xl font-bold mb-4">Welcome</h1>
-      <p>To my simple website, it's an assignment given by sir <b>Ali</b> </p>
+      <p>To my simple website, its an assignment given by sir <b>Ali</b> </p>
     </div>
   );
 }
